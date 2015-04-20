901227e8b9b7968ccde1c02e540441bd485d684b
C.H.A.D.o <chado@chado.by>
2015-04-20 23:35:53 +0300

    MacOS build

--- Source/Blu.Build.cs
+++ Source/Blu.Build.cs
@@ -4,72 +4,90 @@ using System;
 
 public class Blu : ModuleRules
 {
-
-    private string ModulePath
-    {
-        get { return Path.GetDirectoryName(RulesCompiler.GetModuleFilename(this.GetType().Name)); }
-    }
-
-    private string ThirdPartyPath
-    {
-        get { return Path.GetFullPath(Path.Combine(ModulePath, "../ThirdParty/")); }
-    }
-
-	public Blu(TargetInfo Target)
+private string ModulePath
+{
+	get
 	{
+		return Path.GetDirectoryName(RulesCompiler.GetModuleFilename(this.GetType().Name));
+	}
+}
 
-        bool isLibrarySupported = false;
+private string ThirdPartyPath
+{
+	get
+	{
+		return Path.GetFullPath(Path.Combine(ModulePath, "../ThirdParty/"));
+	}
+}
 
-        if ((Target.Platform == UnrealTargetPlatform.Win64) || (Target.Platform == UnrealTargetPlatform.Linux))
-        {
-            isLibrarySupported = true;
+public Blu(TargetInfo Target) {
+	bool isLibrarySupported = false;
 
-
-            /** //// Start Windows platform //// **/
-
-            if ((Target.Platform == UnrealTargetPlatform.Win64))
-            {
-                PublicAdditionalLibraries.Add(Path.Combine(ThirdPartyPath, "cef/Win/lib", "libcef.lib"));
-                PublicAdditionalLibraries.Add(Path.Combine(ThirdPartyPath, "cef/Win/lib", "libcef_dll_wrapper.lib"));
-
-                PublicIncludePaths.AddRange(
-                    new string[] {
-				        Path.Combine(ThirdPartyPath, "cef/Win"),
-                        Path.Combine(ModulePath, "Blu/Public")
-			    });
-            }
-
-            /** //// End Windows platform //// **/
+	if ((Target.Platform == UnrealTargetPlatform.Win64) || (Target.Platform == UnrealTargetPlatform.Linux) || (Target.Platform == UnrealTargetPlatform.Mac)) {
+		isLibrarySupported = true;
 
 
-            /** //// Start Linux 64 platform //// **/
+/** //// Start Windows platform //// **/
 
-            else if ((Target.Platform == UnrealTargetPlatform.Linux))
-            {
+		if ((Target.Platform == UnrealTargetPlatform.Win64)) {
+			PublicAdditionalLibraries.Add(Path.Combine(ThirdPartyPath, "cef/Win/lib", "libcef.lib"));
+			PublicAdditionalLibraries.Add(Path.Combine(ThirdPartyPath, "cef/Win/lib", "libcef_dll_wrapper.lib"));
 
-                PublicAdditionalLibraries.Add(Path.Combine(ThirdPartyPath, "cef/Linux/lib", "libcef.so"));
-                PublicAdditionalLibraries.Add(Path.Combine(ThirdPartyPath, "cef/Linux/lib", "libcef_dll_wrapper.a"));
+			PublicIncludePaths.AddRange(
+			    new string[] {
+					Path.Combine(ThirdPartyPath, "cef/Win"),
+					Path.Combine(ModulePath, "Blu/Public")
+				});
+		}
 
-                PublicIncludePaths.AddRange(
-                    new string[] {
-				        Path.Combine(ThirdPartyPath, "cef/Linux"),
-                        Path.Combine(ModulePath, "Blu/Public")
-			    });
-
-            }
-
-            /** //// End Linux 64 platform //// **/
+/** //// End Windows platform //// **/
 
 
-            /** //// General Libs + Includes //// **/
+/** //// Start Linux 64 platform //// **/
 
-            PrivateIncludePaths.AddRange(
-            new string[] {
+		else if ((Target.Platform == UnrealTargetPlatform.Linux)) {
+			PublicAdditionalLibraries.Add(Path.Combine(ThirdPartyPath, "cef/Linux/lib", "libcef.so"));
+			PublicAdditionalLibraries.Add(Path.Combine(ThirdPartyPath, "cef/Linux/lib", "libcef_dll_wrapper.a"));
+
+			PublicIncludePaths.AddRange(
+			    new string[] {
+					Path.Combine(ThirdPartyPath, "cef/Linux"),
+					Path.Combine(ModulePath, "Blu/Public")
+				});
+		}
+
+/** //// End Linux 64 platform //// **/
+
+
+
+
+/** //// Start MacOS 64 platform //// **/
+
+		else if ((Target.Platform == UnrealTargetPlatform.Mac)) {
+			PublicAdditionalLibraries.Add(Path.Combine(ThirdPartyPath, "cef/Mac/lib", "libplugin_carbon_interpose.dylib"));
+			PublicAdditionalLibraries.Add(Path.Combine(ThirdPartyPath, "cef/Mac/lib", "libcef_dll_wrapper.a"));
+			PublicFrameworks.Add(Path.Combine(ThirdPartyPath, "cef/Mac/lib", "Chromium Embedded Framework.framework"));
+
+			PublicIncludePaths.AddRange(
+			    new string[] {
+					Path.Combine(ThirdPartyPath, "cef/Mac"),
+					Path.Combine(ModulePath, "Blu/Public")
+				});
+		}
+
+/** //// End MacOS 64 platform //// **/
+
+
+/** //// General Libs + Includes //// **/
+
+		PrivateIncludePaths.AddRange(
+		    new string[] {
 				Path.Combine(ModulePath, "Private")
 			});
 
-            PublicDependencyModuleNames.AddRange(
-                new string[]
+
+		PublicDependencyModuleNames.AddRange(
+		    new string[]
 			{
 				"Core",
 				"CoreUObject",
@@ -77,17 +95,13 @@ public class Blu : ModuleRules
 				"InputCore",
 				"RenderCore",
 				"RHI",
-                "Slate",
-                "SlateCore",
-                "UMG",
-                "Json"
+				"Slate",
+				"SlateCore",
+				"UMG",
+				"Json"
 			});
-
-        }
-
-        Definitions.Add(string.Format("WITH_BLUI_BINDING={0}", isLibrarySupported ? 1 : 0));
-
-
 	}
 
+	Definitions.Add(string.Format("WITH_BLUI_BINDING={0}", isLibrarySupported ? 1 : 0));
+}
 }
--- Source/Blu/Private/Blu.cpp
+++ Source/Blu/Private/Blu.cpp
@@ -16,6 +16,9 @@ class FBlu : public IBlu
 
 		BluManager::settings.remote_debugging_port = 7777;
 		
+#if PLATFORM_MAC
+		CefString(&BluManager::settings.browser_subprocess_path).FromASCII("./blu_ue4_process.app/Contents/MacOS/cefsimple");
+#endif
 #if PLATFORM_LINUX
 		CefString(&BluManager::settings.browser_subprocess_path).FromASCII("./blu_ue4_process");
 #endif
